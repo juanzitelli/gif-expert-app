f1dbcfc309f572dead75d1c0d4ea4607f321da3d
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-08-11 18:10:23 -0300

    Added HTTPS to giphy request

--- src/controllers/gifs-controller.js
+++ src/controllers/gifs-controller.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
   )}&limit=10&api_key=fSDvxCITLTqhcjPdBgL340UgQHT8xi26`;
   const response = await fetch(url);
