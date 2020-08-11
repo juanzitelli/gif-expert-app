426e55a7e7887ef5e82ae85a34bb898aa868b9d2
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-08-11 17:50:40 -0300

    Docs added for Github Pages

---
+++ docs/precache-manifest.14b4c78d144dccf8e88f3307882373e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07dc2bf3e467304164bfa80aa13ba8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06b100d46f029e841243",
+    "url": "/static/css/main.6029b665.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b100d46f029e841243",
+    "url": "/static/js/main.6542b539.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
